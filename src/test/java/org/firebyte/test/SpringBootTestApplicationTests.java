9e4146824bd0a3d6f376e0974f816d1721913e50
Jeff Beard <jeff.beard1@gmail.com>
2025-02-14 09:36:58 -0700

    Added Augment generated unit test update.

--- src/test/java/org/firebyte/test/SpringBootTestApplicationTests.java
+++ src/test/java/org/firebyte/test/SpringBootTestApplicationTests.java
@@ -2,12 +2,24 @@ package org.firebyte.test;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class SpringBootTestApplicationTests {
+
+	@SpyBean
+	private ApplicationContext applicationContext;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void mainMethodStartsApplication() {
+		SpringBootTestApplication.main(new String[]{});
+		assertNotNull(applicationContext, "Application context should be created");
+	}
 }
